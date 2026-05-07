14e4d6e830f3c5dfcb5cc1d8767346a7046e3b71
Kyungjong-kim <rudwhd4474@naver.com>
2026-05-07 18:34:49 +0900

    chore: Storybook 온보딩 가이드 비활성화 #20
    - disableWhatsNewNotifications: true
    - features.sidebarOnboardingChecklist: false
    - @chromatic-com/storybook 의존성 제거

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.stories.@(ts|tsx)"],
-  addons: ["@storybook/addon-docs", "@storybook/addon-a11y", "@chromatic-com/storybook"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
+  core: {
+    disableWhatsNewNotifications: true,
+  },
+  features: {
+    sidebarOnboardingChecklist: false,
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
