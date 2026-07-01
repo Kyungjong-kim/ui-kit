94149abc2f49dcf6275c1987fd1ec14f02add830
Kyungjong-kim <rudwhd4474@naver.com>
2026-07-01 18:48:39 +0900

    design: Phase2 규격감사 피드백오버레이 #137

--- src/components/primitives/alert-dialog/alert-dialog.tsx
+++ src/components/primitives/alert-dialog/alert-dialog.tsx
@@ -35,7 +35,7 @@ export function AlertDialog({
         <AlertDialogPrimitive.Overlay className="fixed inset-0 z-50 bg-black/40 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" />
         <AlertDialogPrimitive.Content
           className={cn(
-            "fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded-md bg-[var(--color-bg-primary)] p-inline-xxl shadow-default-lg",
+            "fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-[var(--color-bg-primary)] p-inline-xxl shadow-default-lg",
             "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
             "w-full",
             {
